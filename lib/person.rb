fe7fc13b4a32795d3524717194087efcac5a4459
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-11-10 20:05:32 -0500

    commit tree 7e119ffaff1f1d715ea0761716f4abdcbd3c844b

--- lib/person.rb
+++ lib/person.rb
@@ -8,7 +8,12 @@ class Person
   def initialize(attributes)
 
     attributes.each {|key,value| self.send(("#{key}="), value)}
-    # binding.pry 
+    #We define our initialize method to take in some unspecified attributes object. 
+    #Then, we iterate over each key/value pair in the attributes hash. 
+    #The name of the key becomes the name of a setter method 
+    #and the value associated with the key is the name of the value you want to pass to that method.
+    #The ruby .send method then calls the method name that is the key’s name, with an argument of the value. 
+    binding.pry 
 
   end 
 
